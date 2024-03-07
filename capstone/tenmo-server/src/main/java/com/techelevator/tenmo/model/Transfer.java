3fa63076ef1b864b50a219145ba2b573241c8356
Luke Jordan <lukeajordan23@gmail.com>
2024-03-07 14:40:58 -0500

    Updated Transfer.java

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -1,6 +1,7 @@
 package com.techelevator.tenmo.model;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Transfer {
     private int id;
@@ -9,6 +10,18 @@ public class Transfer {
     private int accountFrom;
     private int accountTo;
     private BigDecimal amount;
+
+    public Transfer() {
+
+    }
+    public Transfer(int id, int transferTypeId, int transferStatusId, int accountFrom, int accountTo, BigDecimal amount) {
+        this.id = id;
+        this.transferTypeId = transferTypeId;
+        this.transferStatusId = transferStatusId;
+        this.accountFrom = accountFrom;
+        this.accountTo = accountTo;
+        this.amount = amount;
+    }
 
     public int getId() {
         return id;
@@ -46,5 +59,29 @@ public class Transfer {
     }
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transfer{" +
+                "id = " + id +
+                ", transferTypeId = " + transferTypeId +
+                ", transferStatusId = " + transferStatusId +
+                ", accountFrom = " + accountFrom +
+                ", accountTo = " + accountTo +
+                ", amount = " + amount +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transfer transfer = (Transfer) o;
+        return id == transfer.id &&
+                transferTypeId == transfer.transferTypeId &&
+                transferStatusId == transfer.transferStatusId &&
+                accountFrom == transfer.accountFrom &&
+                accountTo == transfer.accountTo &&
+                Objects.equals(amount, transfer.amount);
     }
 }
