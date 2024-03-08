4b5d396ab96ad753a5812ed8e4e53d4e109aeeb5
Luke Jordan <lukeajordan23@gmail.com>
2024-03-08 10:43:19 -0500

    Started work on getting the transfers in client side

--- capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -1,11 +1,35 @@
 package com.techelevator.tenmo.services;
 
+import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.util.BasicLogger;
+import org.springframework.http.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransferService {
     private static final String API_BASE_URL = "http://localhost:8080/transfer";
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private static final RestTemplate restTemplate = new RestTemplate();
 
-    
+    public static List<Transfer> getTransfers() {
+        List<Transfer> transfers = new ArrayList<>();
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<Transfer> response = restTemplate.exchange(API_BASE_URL, HttpMethod.GET, entity, Transfer.class);
+            transfers = (List<Transfer>) response;
+        }
+        catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+        return transfers;
+    }
 }
